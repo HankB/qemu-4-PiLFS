d74cd82d3a7c176251fda56cebf273a16e261cfd
Hank Barta <hbarta@gmail.com>
2017-03-13 23:09:33 -0500

    mounts all 3 .IMG partitions

---
+++ qemu.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+# Set up to run raspberry Pi code in preparation for PiLFS (Linux From Scratch.)
+#
+# First step - copy image to a 16GB SSD card, expand file systems (/ to 4096 
+# and add another 8192 partition) And when complete, dd the SD to an .img file
+# 
+
+
+# mount relative to the corrent directory
+export MNT=mnt/rpi
+
+# name of IMG file we'll use
+export IMG=2017-03-02-raspbian-jessie-lite-expanded.img
+
+mkdir -p $MNT
+
+sudo kpartx -a -v $IMG
+sudo mount /dev/mapper/loop0p2 $MNT
+sudo mount /dev/mapper/loop0p1 $MNT/boot
+mkdir -p ${MNT}/lfs
+
+sudo mount /dev/mapper/loop0p3 $MNT/lfs
+
+sudo cp `which qemu-arm-static` ${MNT}/usr/bin/
+
+sudo mount -o bind /dev ${MNT}/dev
+sudo mount -o bind /proc ${MNT}/proc
+sudo mount -o bind /sys ${MNT}/sys
+
+sudo bash -c "echo ':arm:M::\x7fELF\x01\x01\x01\x00\x00\x00\x00\x00\x00\x00\x00\x00\x02\x00\x28\x00:\xff\xff\xff\xff\xff\xff\xff\x00\xff\xff\xff\xff\xff\xff\xff\xff\xfe\xff\xff\xff:/usr/bin/qemu-arm-static:' >/dev/null /proc/sys/fs/binfmt_misc/register"
+
+sudo chroot $MNT
+# and do stuff...
+
+# unwind/clean up
+sudo umount ${MNT}/dev ${MNT}/proc ${MNT}/sys ${MNT}/boot ${MNT}/lfs ${MNT} 
+sudo kpartx -d -v $IMG
